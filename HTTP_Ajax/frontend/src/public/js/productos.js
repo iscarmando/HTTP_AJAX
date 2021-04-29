22673fc0904b63f903dfffbe3647a65813899700
brayan <brayanrodriguezrmz98@gmail.com>
2021-04-29 11:59:24 -0500

    Version que permite eliminar productos

--- HTTP_Ajax/frontend/src/public/js/productos.js
+++ HTTP_Ajax/frontend/src/public/js/productos.js
@@ -1,5 +1,5 @@
 class Producto {
-    constructor(id, nombre,precio,anio){
+    constructor(id,nombre,precio,anio){
         this.id=id;
         this.nombre=nombre;
         this.precio=precio;
@@ -9,17 +9,20 @@ class Producto {
 
 class Interfaz {
     addProduct(product){
+     
+         
         const fila = '<tr id="pr' + product.id + '">   '
-                +    '<td id="nombre'+ product.id + '"> ' + product.nombre + '</td>'
-                +    '<td id="precio'+ product.id + '"> ' + product.precio + '</td>'
-                +    '<td id="año'+ product.id + '"> ' + product.anio + '</td>'
+                +    '<td id="nombre '+ product.nombre + '"> ' + product.nombre + '</td>'
+                +    '<td id="precio '+ product.precio+ '"> ' + product.precio + '</td>'
+                +    '<td id="anio '+ product.anio + '"> ' + product.anio + '</td>'
                 +    '<td>'
                 +    '  <a href="#" class="btn btn-danger btn-sm mt-3" name="borrar"        '
-                +    '  id=delete' + product.id  + '>Borrar</a>'
+                +    '  id='+ product.id  + '>Borrar</a>'
                 +    '  <a href="#" class="btn btn-success btn-sm mt-3" name="editar"        '
-                +    '  id=edit' + product.id  + '>Editar</a>'
+                +    '  id='+ product.id  + '>Editar</a>'
                 +    '</td>'
-                +    '</tr>';
+                +    '</tr>' 
+                ;
 
                 //Producto: id=1, nombre="mouse" precio=120 anio=2021
             $("#listado").append(fila);
@@ -32,6 +35,7 @@ class Interfaz {
 
 
     deleteProduct(id){
+        
 
     }//fin de deleteProduct
 
@@ -76,18 +80,18 @@ $(() =>{
                         console.log(res)
                         res.forEach( (p) =>{
                             const product = new Producto(
-                                p.id, p.nombre, p.precio, p.anio);
-                            interfaz.addProduct(product);
+                                p._id,p.nombre, p.precio, p.anio);
+                                interfaz.addProduct(product);
                         });
                     },
-                    error: () =>{
+                    error:(res) =>{
                         interfaz.message("Error al cargar productos","Error");
 
                     }
                     
-                })
+                })//fin del ajax
             });//fin de cargar los productos de la base de datos
-
+            
             //evento que maneja click en el boton de guardar del formulario
             $("#boton").on('click', function(event){
                 //alert("Funciona");
@@ -121,7 +125,7 @@ $(() =>{
                             "anio":anio
                         },
                         success:(res)=>{
-                            producto.id = res.product.id;
+                            product.id = res.product.id;
                             //aggregamos el producto a la interfaz web
                             interfaz.addProduct(product);
                             interfaz.message("Producto agregado correctamente");
@@ -136,6 +140,58 @@ $(() =>{
 
 
                 }//else
-            })
+            })//fin del evento del boton agregar producto
 
-})
+            
+            $("#listado").on('click', '.btn.btn-danger', (event)=>{
+                //obtener el id del boton que se hizo click
+                
+                var id  = $(".btn.btn-danger").attr('id');
+                alert(id)
+                $.ajax({
+                    url:'http://localhost:3000/api/productos/'+id,
+                    method: 'DELETE',
+                    headers: {
+                        'ContentType': 'Application/JSON'
+                    },
+                    success: (res)=> {
+                    
+                        interfaz.message("Producto elminiado",'warnign')
+                        
+                    },
+                    error:(res) =>{
+                        interfaz.message("Ocurrio un error",'danger')
+                    }
+                });//fin del ajax
+               
+
+            })//fin del evento borrar
+
+            //evento update
+            $("#listado").on('click', '.btn.btn-success', (event)=>{
+                //obtener el id del boton que se hizo click
+                
+                var id  = $(".btn.btn-success").attr('id');
+                alert(id)
+                $.ajax({
+                    url:'http://localhost:3000/api/productos/'+id,
+                    method: 'POST',
+                    headers: {
+                        'ContentType': 'Application/JSON'
+                    },
+                    success: (res)=> {
+                    
+                        interfaz.message("Producto elminiado",'warnign')
+                        
+                    },
+                    error:(res) =>{
+                        interfaz.message("Ocurrio un error",'danger')
+                    }
+                });//fin del ajax
+               
+
+            })//fin del evento update
+
+
+ })
+       
